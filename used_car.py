850e8875bb3d4c7c70d2fb9e83218ec30f1a514b
Pankaj <pankajbh860@gmail.com>
2024-09-18 21:50:07 +0530

    first commit

--- used_car.py
+++ used_car.py
@@ -1,3 +1,34 @@
-print('hello my na;me is pankaj')
-for i in range(10):
-    print(i)
+import os
+import pymysql.cursors
+from dotenv import load_dotenv
+import pandas as pd 
+
+load_dotenv()
+
+host = os.getenv('HOST')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+database = os.getenv('DATABASE')
+
+connection = pymysql.connect(user=user,
+host = host,
+password =password,
+database =database)
+
+cursor = connection.cursor()
+
+def select_query(query):
+    
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=[column[0] for column in cursor.description])
+    
+    return df
+
+def show_talbes():
+    query = 'show_tables;'
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=[column[0] for column in cursor.description])
+    
+    return df
